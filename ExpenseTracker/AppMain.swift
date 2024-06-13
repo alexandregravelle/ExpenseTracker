2e1df0952dc860729df057069c8eaec6751c91ea
Alexandre Gravelle <agrav065@gmail.com>
2024-06-13 11:35:47 -0400

    Invoking Single Responsibility
    - Add new file Persisitance.swift in Storage.
    - Define a new struct called PersistanceController and declare a static constant of a shared instance.
    - Initialize a persistance container which takes in a inMemory parameter for showing fake data in SwiftUI Previews.
    - Fix typo on Persistance.swift to Persistence.swift.
    - Add a static variable preview for a in-memory implementation of the persistence controller.
    - Add static variable previewItem of type ExpenseModel.
    - Remove static variable container, previewContainer and previewItem in AppMain and updates instances to use PersistenceController.
    - Remove unused import CoreData.

--- ExpenseTracker/AppMain.swift
+++ ExpenseTracker/AppMain.swift
@@ -31,57 +31,9 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-import CoreData
 
 @main
 struct AppMain: App {
-  static var container: NSPersistentContainer = {
-    let container = NSPersistentContainer(name: "ExpensesModel")
-    container.loadPersistentStores { _, error in
-      if let error = error as NSError? {
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    }
-    return container
-  }()
-
-  static var previewContainer: NSPersistentContainer = {
-    let container = NSPersistentContainer(name: "ExpensesModel")
-    container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
-    container.loadPersistentStores { _, error in
-      if let error = error as NSError? {
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    }
-
-    for index in 0..<5 {
-      let newItem = ExpenseModel(context: container.viewContext)
-      newItem.title = "Test Title \(index)"
-      newItem.date = Date(timeIntervalSinceNow: Double(index * 60))
-      newItem.comment = "Test Comment \(index)"
-      newItem.price = Double(index + 1) * 12.3
-      newItem.id = UUID()
-    }
-    do {
-      try container.viewContext.save()
-    } catch {
-      let nsError = error as NSError
-      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-    }
-
-    return container
-  }()
-
-  static let previewItem: ExpenseModel = {
-    let newItem = ExpenseModel(context: AppMain.previewContainer.viewContext)
-    newItem.title = "Preview Item Title"
-    newItem.date = Date(timeIntervalSinceNow: 60)
-    newItem.comment = "Preview Item Comment"
-    newItem.price = 12.34
-    newItem.id = UUID()
-    return newItem
-  }()
-
   var body: some Scene {
     WindowGroup {
       ContentView()
